b1b5e31a615f2296ef69ed57aee35e78fa3a1789
unknown <travisbostick@gmail.com>
2020-11-17 19:18:38 -0600

    initial commit with rough copy of ERD and schema

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,12 @@
+
+
+--creates the table for the departments csv
+CREATE TABLE departments_tb(
+		dept_no VARCHAR(4)
+		dept_name VARCHAR(30)
+)
+--creates the table for the dept_emp csv
+--creates the table for the dept_manager csv
+--creates the table for the employees csv
+--creates the table for the salaries csv
+--creates the table for the titles csv
